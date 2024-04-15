5ee0593ac58a615d1dd443c5436514af460f3694
IstudorDenis <167008397+IstudorDenis@users.noreply.github.com>
2024-04-15 22:05:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-document.getElementsByTagName("p")[0].style.fontSize = "3vw";
+document.getElementsByTagName("p")[0].style.fontSize = "4vw";
 
 document.getElementById("2").classList.add('active');
 
